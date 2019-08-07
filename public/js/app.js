84254a6e7a9f3d97501c726b6b077d743f68ba81
Rodrigo Chamma Castro <rodrigochamma@gmail.com>
2019-08-06 20:15:51 -0700

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault() //stop the refresh
+
+    const location = search.value
+    
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    //console.log(response)
+    response.json().then((data) => {
+        if(data.error)
+        messageOne.textContent = data.error
+        else{
+            messageOne.textContent = 'Location: ' + data.location
+            messageTwo.textContent = 'Forecast: ' + data.forecast
+        }
+    })
+})
+})
